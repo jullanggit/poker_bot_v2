d0f6d0585af6525e0335e0ac2b1c603e3d3fa8c9
jullang <jullanggit@proton.me>
2024-12-09 10:31:23 +0100

    move main to lib

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+pub mod io;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Card {
+    value: CardValue,
+    color: Color,
+}
+impl Card {
+    pub fn new(value: CardValue, color: Color) -> Self {
+        Self { value, color }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub enum CardValue {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum Color {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Clone, Copy)]
+pub enum Hand {
+    RoyalFlush,
+    StraightFlush,
+    FourOfAKind,
+    FullHouse,
+    Flush,
+    Straight,
+    ThreeOfAKind,
+    TwoPair,
+    Pair,
+    HighCard,
+}
+
+struct Results {
+    wins: u64,
+    draws: u64,
+    losses: u64,
+}
+
+fn calculate<const NUM_CARDS: usize>(cards: &[Card; NUM_CARDS]) -> Results {
+    let (player_cards, present_pool) = cards.split_at(2);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,5 @@
-mod io;
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct Card {
-    value: CardValue,
-    color: Color,
-}
-impl Card {
-    pub fn new(value: CardValue, color: Color) -> Self {
-        Self { value, color }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
-pub enum CardValue {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
-
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
-pub enum Color {
-    Hearts,
-    Diamonds,
-    Clubs,
-    Spades,
-}
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Clone, Copy)]
-pub enum Hand {
-    RoyalFlush,
-    StraightFlush,
-    FourOfAKind,
-    FullHouse,
-    Flush,
-    Straight,
-    ThreeOfAKind,
-    TwoPair,
-    Pair,
-    HighCard,
-}
+use poker_bot_v2::io::get_cards;
 
 fn main() {
-    println!("Hello, world!");
+    let cards = get_cards().unwrap();
 }

ba8340862cc0485ea245c5b76d16d8dd6433ce27
jullang <jullanggit@proton.me>
2024-12-10 15:27:04 +0100

    import match_lan_calculate from v1 (to transform runtime values to a set of const generics)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use poker_bot_v2::io::get_cards;
+use poker_bot_v2::{calculate, io::get_cards};
 
+/// Runs calculate with the given const lens
+macro_rules! match_len_calculate {
+    ($present_cards:expr, $($num:expr),+) => {
+        match $present_cards.len() {
+            $(
+                $num => {
+                    let present_cards = $present_cards.try_into().unwrap();
+                    calculate::<$num>(present_cards)
+                }
+            ),+
+            _ => unreachable!()
+        }
+    };
+}
 fn main() {
     let cards = get_cards().unwrap();
+    let results = match_len_calculate!(cards, 2, 3, 4, 5, 6, 7);
 }

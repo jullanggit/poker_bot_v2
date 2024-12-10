7052e632f696c9ca7b55fca5152011387572d7bd
jullang <jullanggit@proton.me>
2024-12-10 15:22:51 +0100

    rename const generics to fit nCr

--- src/combinations.rs
+++ src/combinations.rs
@@ -3,14 +3,14 @@ use std::array;
 use crate::Card;
 
 #[derive(Clone)]
-pub struct CardCombinations<'a, const N: usize, const K: usize> {
-    indices: [usize; K],
+pub struct CardCombinations<'a, const N: usize, const R: usize> {
+    indices: [usize; R],
     cards: &'a [Card; N],
     first: bool,
 }
-impl<'a, const N: usize, const K: usize> CardCombinations<'a, N, K> {
+impl<'a, const N: usize, const R: usize> CardCombinations<'a, N, R> {
     pub fn new(cards: &'a [Card; N]) -> Self {
-        assert!(K <= N);
+        assert!(R <= N);
 
         Self {
             indices: array::from_fn(|index| index),
@@ -18,35 +18,25 @@ impl<'a, const N: usize, const K: usize> CardCombinations<'a, N, K> {
             first: true,
         }
     }
-    fn get_current_combination(&self) -> [Card; K] {
+    fn get_current_combination(&self) -> [Card; R] {
         array::from_fn(|index| self.cards[self.indices[index]])
     }
 }
-impl<const N: usize, const K: usize> Iterator for CardCombinations<'_, N, K> {
-    type Item = [Card; K];
+impl<const N: usize, const R: usize> Iterator for CardCombinations<'_, N, R> {
+    type Item = [Card; R];
     fn next(&mut self) -> Option<Self::Item> {
         if self.first {
             self.first = false;
             return Some(self.get_current_combination());
         }
 
-        // Increment the indices
-        let mut indices_index = K - 1;
-
-        // Decrement indices index to find one that isnt at its maximum allowed value
-        while self.indices[indices_index] == indices_index + N - K {
-            if indices_index > 0 {
-                indices_index -= 1;
-            } else {
-                // Last combination reached
-                return None;
-            }
+        if increment_indices::<N, R>(&mut self.indices) {
+            None
+        } else {
+            Some(self.get_current_combination())
         }
-        // Increment the found index
-        self.indices[indices_index] += 1;
-        // And reset the ones to its right
-        for right_index in indices_index + 1..K {
-            self.indices[right_index] = self.indices[right_index - 1] + 1;
+    }
+}
         }
 
         // Return the current combination

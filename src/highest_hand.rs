9fe907d967fa9086def562c2b32b5a6fdb2d1893
Jul Lang <jullanggit@proton.me>
2025-01-01 15:10:56 +0100

    Add typestate to ValueBitmap to indicate wheter it represents a single color or multiple ones

--- src/highest_hand.rs
+++ src/highest_hand.rs
@@ -1,21 +1,38 @@
 use crate::{Card, Hand};
+use std::marker::PhantomData;
+
+#[derive(Clone, Copy)]
+struct SingleColored;
+#[derive(Clone, Copy)]
+struct MultiColored;
 
 #[derive(Clone, Copy)]
 /// A bitmap with one bit per `CardValue`
-struct ValueBitmap(u16);
-impl ValueBitmap {
+struct ValueBitmap<State> {
+    inner: u16,
+    _state: PhantomData<State>,
+}
+impl ValueBitmap<SingleColored> {
     fn is_flush(&self) -> bool {
-        self.0.count_ones() >= 5
+        self.inner.count_ones() >= 5
     }
+}
+impl<State> ValueBitmap<State> {
     fn is_straight(&self) -> bool {
         let straight_mask = 0b0001111100000000;
         for shift in 0..9 {
             let mask = straight_mask >> shift;
-            if self.0 & mask == mask {
+            if self.inner & mask == mask {
                 return true;
             }
         }
         false
+    }
+    fn new(inner: u16) -> Self {
+        Self {
+            inner,
+            _state: PhantomData,
+        }
     }
 }
 
@@ -29,11 +46,11 @@ pub fn highest_hand(cards: [Card; 7]) -> Hand {
     todo!()
 }
 
-fn convert_to_color_bitmaps(cards: [Card; 7]) -> [ValueBitmap; 4] {
-    let mut color_bitmaps = [ValueBitmap(0); 4];
+fn convert_to_color_bitmaps(cards: [Card; 7]) -> [ValueBitmap<SingleColored>; 4] {
+    let mut color_bitmaps = [ValueBitmap::new(0); 4];
 
     for card in cards {
-        color_bitmaps[card.color as usize].0 |= 1 << card.value as u8;
+        color_bitmaps[card.color as usize].inner |= 1 << card.value as u8;
     }
 
     color_bitmaps
